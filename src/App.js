fcff5bdb69e025d32d7f0843701b123823548d80
Victoria Matynyan <the.strange.creature@yandex.ru>
2023-11-07 15:50:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Фронтенд находится в разработке. Скоро здесь будет красиво!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
