67d0f297183ba1fb7946023615dd69669fddd838
Victoria Matynyan <the.strange.creature@yandex.ru>
2023-11-23 16:56:23 +0300

    add and set up: MoviesApi and MainApi

---
+++ src/utils/MainApi.js
@@ -0,0 +1,136 @@
+import { BASE_URL } from './urls';
+
+export function validateResponse(res) {
+    if(res.ok) {
+        return res.json();
+    }
+    return Promise.reject(`Ошибка получения ответа от сервера: ${res.status}`)
+};
+
+export const register = (name, email, password) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email, password })
+    })
+    .then(validateResponse);
+};
+
+export const login = (email, password) => {
+    return fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        credentials: 'include', // отправляем куки вместе с запросом
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+    .then(validateResponse);
+};
+
+export const logout = () => {
+    return fetch(`${BASE_URL}/signout`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(validateResponse);
+};
+
+// запрос на данные текущего пользователя
+export const getUserInfo = () => {
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(validateResponse);
+};
+
+// запрос на изменение данных пользователя
+export const editUserInfo = (name, email) => { //formValues
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'PATCH',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email }) //formValues.name, //formValues.email
+    })
+    .then(validateResponse);
+};
+
+// запрос карточек с фильмами
+export const getMovies = () => {
+    return fetch(`${BASE_URL}/movies`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(validateResponse);
+};
+
+// запрос на сохранение карточки
+export const addMovie = (
+    nameRU,
+    nameEN,
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+) => {
+    return fetch(`${BASE_URL}/movies`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nameRU,
+            nameEN,
+            country,
+            director,
+            duration,
+            year,
+            description,
+            image,
+            trailerLink,
+            thumbnail,
+            movieId,
+        })
+    })
+    .then(validateResponse);
+};
+
+// запрос на удаление карточки из списка сохранённых
+export const deleteMovie = (movieId) => {
+    return fetch(`${BASE_URL}/movies/${movieId}`, {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(validateResponse);
+}
---
+++ src/utils/urls.js
@@ -0,0 +1,3 @@
+export const BEATFILM_BASE_URL = 'https://api.nomoreparties.co/';
+export const BEATFILM_MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+export const BASE_URL = 'https://api.moviepub.nomoredomainsmonster.ru';
